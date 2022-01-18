911f28fe7e94be0b0d264aea0592af01fb6b6cb2
Sebastian Rojas Cabal <srojascabal@gmail.com>
2022-01-18 16:02:23 -0500

    Data exploration

--- Code Files/0a. Exploration 990 Data.R
+++ Code Files/0a. Exploration 990 Data.R
@@ -10,6 +10,7 @@
 
 # Preliminaries
 library(tidyverse)
+library(lubridate)
 library(readxl)
 
 # Import data
@@ -23,6 +24,33 @@ library(readxl)
 sched_f_activities <- read_csv("/Users/srojascabal/Google Drive/F990/Data from OneDrive/sched_f_activities.csv")
 sched_f_individ_grants <- read_csv("/Users/srojascabal/Google Drive/F990/Data from OneDrive/sched_f_individ_grants.csv")
 
+# Known anti-LGBTQ orgs
+antilgbt <- read_excel("/Users/srojascabal/Google Drive/F990/Data from OneDrive/Select Anti-LGBT+ Organizations - Foreign Expenditures.xlsx") %>%
+    distinct(name, ein) %>%
+    mutate(anti = 1,
+           ein2 = ein,
+           ein = as.numeric(str_remove_all(ein2, "-"))) # removed the - in the string in order to make the records compatible
+
+# Header
+header <- read_csv("/Users/srojascabal/Google Drive/F990/Data from OneDrive/return_header.csv")
+  header_select <- select(header, ein, RtrnHdr_RtrnTs, RtrnHdr_TxPrdEndDt, RtrnHdr_TxPrdBgnDt) %>% # only the vars we need
+  mutate(fiscal_year = year(RtrnHdr_TxPrdEndDt),
+         hour_submission = hour(RtrnHdr_RtrnTs)) # some changes on the year/time stamps, for future cleaning work
+  
+# Header with anti-lgbtq indicator
+  header_anti <- full_join(header_select, antilgbt, by = c("ein")) %>%
+    mutate(anti = case_when(
+      anti == 1 ~ 1,
+      TRUE ~ 0),
+      anti_factor = as_factor(case_when(anti == 1 ~ "Anti-LGBTQ+",
+                                        anti == 0 ~ "Other NGOs")))
+  
+  test_nodups <- header_anti %>%
+    mutate(FileTs = RtrnHdr_RtrnTs) %>%
+    group_by(ein, fiscal_year) %>%
+    slice_max(order_by = FileTs, n = 1) # Removes multiple submissions in a single tax year. Keeps the most recent. 20438 obs removed.
+                                
+  
 # Part 0 - Basic info
 part_0 <- read_csv("/Users/srojascabal/Google Drive/F990/Data from OneDrive/part_0.csv")
   test <- slice(part_0, 1:5)
@@ -50,6 +78,6 @@ where_activities <- select(sched_f_activities, RgnTxt) %>%
   distinct() # over 3k unique places. That's a lot!
 
 
-# SO NEXT STEPS: RETURN HEADER TO IDENTIFY ORGS AND FILING YEARS, MERGE WITH LGBTQ REGISTRY TO CREATE "ANTI" DUMMY,
+# SO NEXT STEPS: RETURN HEADER TO IDENTIFY ORGS AND FILING YEARS,
 # THEN, GO ON PART IV TO SEE IF THEY CLAIM TO GIVE MONEY ABROAD, GET THE % OF EXPENDETURES ABROAD
 # TOTAL GRANTS, TOTAL MONEY SPENT ABROAD, WHERE?

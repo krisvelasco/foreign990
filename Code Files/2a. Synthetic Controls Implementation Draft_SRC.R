6c12094a5c0f25208ae13ac2515072198630f7b6
Sebastian Rojas Cabal <srojascabal@gmail.com>
2022-05-20 15:38:22 -0500

    SCM implementation.

--- Code Files/2a. Synthetic Controls Implementation Draft_SRC.R
+++ Code Files/2a. Synthetic Controls Implementation Draft_SRC.R
@@ -1,8 +1,21 @@
 ## Project: Nonprofit Foreign Expenditures
 
 ## Overview: 
-##   Implementing models with synthetic controls. First draft.
+##  Implementing models with synthetic controls. First draft.
 
 #Last updated: May 20 by Sebastian Rojas Cabal
 
+#--------------------------------------------------------
+# Preliminaries
+#   Loading packages
+#--------------------------------------------------------
+
+library(magrittr)
+library(dplyr)
+library(augsynth)
+
+library(tidyverse)
+library(lubridate)
+library(readxl)
+library(stringr)
 #--------------------------------------------------------

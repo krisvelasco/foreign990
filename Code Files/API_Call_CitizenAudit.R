e63ccae52e2b69e2bfa235f84b6872f14135596b
krisvelasco <kvelasco8@gmail.com>
2022-06-03 10:23:43 -0400

    API Code

---
+++ Code Files/API_Call_CitizenAudit.R
@@ -0,0 +1,81 @@
+## File for Downloading Specified PDFs from ProPublica API
+# OPIOID
+# 12.17.20
+# Andrew Messamore
+
+library( RCurl )
+library( jsonlite )
+library( dplyr )
+library( pander )
+library(readxl)
+
+rm(list=ls())
+
+
+## Create Directory
+setwd("/Users/Wolfalev/Documents/Grad/UT-AUSTIN GRAD/Articles and Projects/5. 990 Project/")
+dir.create( "IRS990_PDFs_Opioid" )
+setwd( "./IRS990_PDFs_Opioid" )
+
+
+## Download Data #Change Link Below
+opioid_eins  <- read_excel("/Users/Wolfalev/Documents/Grad/UT-AUSTIN GRAD/Articles and Projects/5. 990 Project/Data_Main/eins_opioid_19982016.xlsx")
+opioid_eins <- opioid_eins %>% 
+  mutate(ein_org = as.character(ein_org))
+
+## Write Function
+download_pdfs <- function( ein )
+{
+  
+  URL <- paste0( "https://projects.propublica.org/nonprofits/api/v2/organizations/", ein, ".json" )
+  
+  results.json <- getURL( URL, ssl.verifypeer = FALSE )
+  
+  r2 <- gsub( "\n", "", results.json )
+  
+  d <- fromJSON( r2 )
+  
+  pdfs <- d$filings_without_data
+  
+  if( nrow(pdfs) > 0 )
+  {
+    for( i in 1:nrow(pdfs) )
+    {  
+      
+      file.name <- paste0( "EIN_", ein , "_YEAR_", pdfs$tax_prd_yr[i], "_FORMTYPE_", pdfs$formtype_str[i],  ".pdf" )
+      download.file( url=pdfs$pdf_url[i], 
+                     destfile=file.name,
+                     mode = "wb")
+  }
+  
+  return( dir() )
+  
+  }
+
+}
+
+
+## Pull PDFS
+
+error_counter <- 0
+# 18 errors
+
+for (i in opioid_eins$ein_org){
+  
+  skip_to_next <- FALSE
+  
+  tryCatch(download_pdfs(i), error = function(e) { skip_to_next <<- TRUE})
+  
+  if(skip_to_next){ 
+    print("Error Found")
+    error_counter <- error_counter +1
+    next
+  }
+  
+  Sys.sleep(30)
+}
+
+
+
+
+

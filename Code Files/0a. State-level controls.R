2730ba7bfd5c78d7e537bdbdf9861634bf1e731b
Sebastian Rojas Cabal <srojascabal@gmail.com>
2022-06-11 10:56:10 -0400

    State-level control variables.

---
+++ Code Files/0a. State-level controls.R
@@ -0,0 +1,10 @@
+# State-level controls
+# Prepared by Sebastian Rojas for the F990 project
+
+#Packages
+library(tidyverse)
+
+# Data import
+#   State-level GDP, 2008-2015.
+#     Source: Bureau of Economic Analysis (https://apps.bea.gov/regional/histdata/releases/0616gsp/index.cfm)
+read_csv()

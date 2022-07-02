f4d19082f569405c2b6b82bc8c1698955aa184b0
Sebastian Rojas Cabal <srojascabal@gmail.com>
2022-07-02 09:32:58 +0200

    Updated the data cleaning.

--- Code Files/1. Cleaning aggregated data_Form 990 and anti LGBTQ indicator.R
+++ Code Files/1. Cleaning aggregated data_Form 990 and anti LGBTQ indicator.R
@@ -34,6 +34,23 @@ dups <- dirty_data %>% count(ein, tax_year) %>%
   group_by(ein, tax_year) %>%
   select(ein, tax_year, dup)
 
+dup_data <- joindf_dups %>%
+  select(
+    ein, tax_year, rtrn_timestmp, dup
+  ) %>%
+  arrange(
+    ein, tax_year, rtrn_timestmp, dup
+  ) %>%
+  filter(
+    dup == 1
+  )
+
+sliced_min_dup <- dup_data %>%
+  slice_min(rtrn_timestmp)
+
+max_money <- dirty_data %>%
+  filter(pt9_totalFrgnGrnts == 4160760803)
+
 # Adding them to the joint data
 joindf_dups <- inner_join(dirty_data, dups, by = c("ein", "tax_year"))
 #--------------------------
@@ -89,7 +106,7 @@ frgnxpns <- joindf_dups %>%
 #-----
 # ANTI-LBTQ+ sample: New vars
 #-----
-anti_frgnxpns <- frgnxpns %>%
+anti_frgnxpns_clean <- frgnxpns %>%
   filter(anti_lgbtq == 1) %>%
   group_by(ein, tax_year) %>%
   slice_min(rtrn_timestmp) %>%
@@ -231,214 +248,208 @@ anti_frgnxpns <- frgnxpns %>%
       pt0_state == "WV" ~ 2014,
       pt0_state == "WI" ~ 2014,
       pt0_state == "WY" ~ 2014
-    )
+    ),
+    ind_yearMrgEq_rtrn  = case_when(
+      tax_year < yearMrgEq_rtrn ~ 0,
+      TRUE ~ 1
+    ),
+    ind_yearMrgEq_pt0  = case_when(
+      tax_year < yearMrgEq_pt0 ~ 0,
+      TRUE ~ 1
+    ),
   ) %>%
-  filter(tax_year < 2021)
-  
-group_by(rtrn_state) %>%
-  mutate(
-    indicMrgEq_rtrn  = case_when(
-      yearMrgEq_rtrn >= tax_year ~ 1,
-      FALSE ~ 0
-    )
-  )
-  
-#--------
+  filter(tax_year < 2021) %>%
+  ungroup()
+#----
 #-----
 # NON ANTI-LBTQ+ sample: New vars
 #   We'll split the sample into smaller chunks to make computation easier
 #-----
 nonanti_frgnxpns <- frgnxpns %>%
   filter(anti_lgbtq == 0)
-#--------
-# WHAT WE SEE BELOW COULD HAVE BEEN A LOOP
-#--------
-# 2013
-#--------
-nonanti_frgnxpns2013_1 <- nonanti_frgnxpns %>%
-  filter(tax_year == 2013) %>%
-  slice(seq(0.5 * n())) %>%
-  group_by(ein, tax_year) %>%
-  slice_min(rtrn_timestmp) %>%
-  mutate(
-    totalXpns_2013 = totalXpns/(232.957/232.957),
-    frgnXpns_2013 = frgnXpns/(232.957/232.957))
 
-nonanti_frgnxpns2013_2 <- nonanti_frgnxpns %>%
-  filter(tax_year == 2013) %>%
-  slice(-seq(0.5 * n())) %>%
-  group_by(ein, tax_year) %>%
-  slice_min(rtrn_timestmp) %>%
-  mutate(
-    totalXpns_2013 = totalXpns/(232.957/232.957),
-    frgnXpns_2013 = frgnXpns/(232.957/232.957))
-#--------
-#--------
-# 2014
-#--------
-nonanti_frgnxpns2014_1 <- nonanti_frgnxpns %>%
-  filter(tax_year == 2014) %>%
-  slice(seq(0.5 * n())) %>%
-  group_by(ein, tax_year) %>%
-  slice_min(rtrn_timestmp) %>%
-  mutate(
-    totalXpns_2013 = totalXpns/(232.957/236.736),
-    frgnXpns_2013 = frgnXpns/(232.957/236.736))
+tax_year_vector <- c(2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020)
+cpi_vector <- c(232.957, 236.736, 237.017, 240.007, 245.120, 251.107, 255.657, 258.811)
 
-nonanti_frgnxpns2014_2 <- nonanti_frgnxpns %>%
-  filter(tax_year == 2014) %>%
-  slice(-seq(0.5 * n())) %>%
-  group_by(ein, tax_year) %>%
-  slice_min(rtrn_timestmp) %>%
-  mutate(
-    totalXpns_2013 = totalXpns/(232.957/236.736),
-    frgnXpns_2013 = frgnXpns/(232.957/236.736))
-#--------
-#--------
-# 2015
-#--------
-nonanti_frgnxpns2015_1 <- nonanti_frgnxpns %>%
-  filter(tax_year == 2015) %>%
-  slice(seq(0.5 * n())) %>%
-  group_by(ein, tax_year) %>%
-  slice_min(rtrn_timestmp) %>%
-  mutate(
-    totalXpns_2013 = totalXpns/(232.957/237.017),
-    frgnXpns_2013 = frgnXpns/(232.957/237.017))
+for (i in 1:length(tax_year_vector)) {
 
-nonanti_frgnxpns2015_2 <- nonanti_frgnxpns %>%
-  filter(tax_year == 2015) %>%
-  slice(-seq(0.5 * n())) %>%
-  group_by(ein, tax_year) %>%
-  slice_min(rtrn_timestmp) %>%
-  mutate(
-    totalXpns_2013 = totalXpns/(232.957/237.017),
-    frgnXpns_2013 = frgnXpns/(232.957/237.017))
-#--------
-#--------
-# 2016
-#--------
-nonanti_frgnxpns2016_1 <- nonanti_frgnxpns %>%
-  filter(tax_year == 2016) %>%
-  slice(seq(0.5 * n())) %>%
-  group_by(ein, tax_year) %>%
-  slice_min(rtrn_timestmp) %>%
-  mutate(
-    totalXpns_2013 = totalXpns/(232.957/240.007),
-    frgnXpns_2013 = frgnXpns/(232.957/240.007))
+# Subsetting non-anti data by year, adding and cleaning variables
+#----  
+  df_subset <- nonanti_frgnxpns %>%
+    filter(tax_year == tax_year_vector[i]) %>%
+    group_by(ein, tax_year) %>%
+    slice_min(rtrn_timestmp) %>%
+    mutate(
+      totalXpns_2013 = totalXpns/(232.957/cpi_vector[i]),
+      frgnXpns_2013 = frgnXpns/(232.957/cpi_vector[i]),
+    yearMrgEq_rtrn = case_when(
+      rtrn_state == "AL" ~ 2015,
+      rtrn_state == "AK" ~ 2014,
+      rtrn_state == "AZ" ~ 2014,
+      rtrn_state == "AR" ~ 2015,
+      rtrn_state == "AS" ~ 2014,
+      rtrn_state == "CA" ~ 2013,
+      rtrn_state == "CO" ~ 2014,
+      rtrn_state == "CT" ~ 2008,
+      rtrn_state == "DE" ~ 2013,
+      rtrn_state == "DC" ~ 2010,
+      rtrn_state == "FL" ~ 2015,
+      rtrn_state == "GA" ~ 2015,
+      rtrn_state == "GU" ~ 2015,
+      rtrn_state == "HI" ~ 2013,
+      rtrn_state == "ID" ~ 2014,
+      rtrn_state == "IL" ~ 2013,
+      rtrn_state == "IN" ~ 2014,
+      rtrn_state == "IA" ~ 2009,
+      rtrn_state == "KS" ~ 2015,
+      rtrn_state == "KY" ~ 2015,
+      rtrn_state == "LA" ~ 2015,
+      rtrn_state == "ME" ~ 2012,
+      rtrn_state == "MD" ~ 2012,
+      rtrn_state == "MA" ~ 2004,
+      rtrn_state == "MI" ~ 2015,
+      rtrn_state == "MN" ~ 2013,
+      rtrn_state == "MS" ~ 2015,
+      rtrn_state == "MO" ~ 2015,
+      rtrn_state == "MT" ~ 2014,
+      rtrn_state == "NE" ~ 2015,
+      rtrn_state == "NV" ~ 2014,
+      rtrn_state == "NH" ~ 2010,
+      rtrn_state == "NJ" ~ 2013,
+      rtrn_state == "NM" ~ 2013,
+      rtrn_state == "NY" ~ 2011,
+      rtrn_state == "NC" ~ 2014,
+      rtrn_state == "ND" ~ 2015,
+      rtrn_state == "CM" ~ 2015,
+      rtrn_state == "OH" ~ 2015,
+      rtrn_state == "OK" ~ 2014,
+      rtrn_state == "OR" ~ 2014,
+      rtrn_state == "PA" ~ 2014,
+      rtrn_state == "PR" ~ 2015,
+      rtrn_state == "RI" ~ 2013,
+      rtrn_state == "SC" ~ 2014,
+      rtrn_state == "SD" ~ 2015,
+      rtrn_state == "TN" ~ 2015,
+      rtrn_state == "TX" ~ 2015,
+      rtrn_state == "UT" ~ 2015,
+      rtrn_state == "VT" ~ 2014,
+      rtrn_state == "VA" ~ 2009,
+      rtrn_state == "VI" ~ 2014,
+      rtrn_state == "WA" ~ 2012,
+      rtrn_state == "WV" ~ 2014,
+      rtrn_state == "WI" ~ 2014,
+      rtrn_state == "WY" ~ 2014
+        ),
+    yearMrgEq_pt0 = case_when(
+      pt0_state == "AL" ~ 2015,
+      pt0_state == "AK" ~ 2014,
+      pt0_state == "AZ" ~ 2014,
+      pt0_state == "AR" ~ 2015,
+      pt0_state == "AS" ~ 2014,
+      pt0_state == "CA" ~ 2013,
+      pt0_state == "CO" ~ 2014,
+      pt0_state == "CT" ~ 2008,
+      pt0_state == "DE" ~ 2013,
+      pt0_state == "DC" ~ 2010,
+      pt0_state == "FL" ~ 2015,
+      pt0_state == "GA" ~ 2015,
+      pt0_state == "GU" ~ 2015,
+      pt0_state == "HI" ~ 2013,
+      pt0_state == "ID" ~ 2014,
+      pt0_state == "IL" ~ 2013,
+      pt0_state == "IN" ~ 2014,
+      pt0_state == "IA" ~ 2009,
+      pt0_state == "KS" ~ 2015,
+      pt0_state == "KY" ~ 2015,
+      pt0_state == "LA" ~ 2015,
+      pt0_state == "ME" ~ 2012,
+      pt0_state == "MD" ~ 2012,
+      pt0_state == "MA" ~ 2004,
+      pt0_state == "MI" ~ 2015,
+      pt0_state == "MN" ~ 2013,
+      pt0_state == "MS" ~ 2015,
+      pt0_state == "MO" ~ 2015,
+      pt0_state == "MT" ~ 2014,
+      pt0_state == "NE" ~ 2015,
+      pt0_state == "NV" ~ 2014,
+      pt0_state == "NH" ~ 2010,
+      pt0_state == "NJ" ~ 2013,
+      pt0_state == "NM" ~ 2013,
+      pt0_state == "NY" ~ 2011,
+      pt0_state == "NC" ~ 2014,
+      pt0_state == "ND" ~ 2015,
+      pt0_state == "CM" ~ 2015,
+      pt0_state == "OH" ~ 2015,
+      pt0_state == "OK" ~ 2014,
+      pt0_state == "OR" ~ 2014,
+      pt0_state == "PA" ~ 2014,
+      pt0_state == "PR" ~ 2015,
+      pt0_state == "RI" ~ 2013,
+      pt0_state == "SC" ~ 2014,
+      pt0_state == "SD" ~ 2015,
+      pt0_state == "TN" ~ 2015,
+      pt0_state == "TX" ~ 2015,
+      pt0_state == "UT" ~ 2015,
+      pt0_state == "VT" ~ 2014,
+      pt0_state == "VA" ~ 2009,
+      pt0_state == "VI" ~ 2014,
+      pt0_state == "WA" ~ 2012,
+      pt0_state == "WV" ~ 2014,
+      pt0_state == "WI" ~ 2014,
+      pt0_state == "WY" ~ 2014
+      ),
+    ind_yearMrgEq_rtrn  = case_when(
+      tax_year < yearMrgEq_rtrn ~ 0,
+      TRUE ~ 1
+      ),
+    ind_yearMrgEq_pt0  = case_when(
+      tax_year < yearMrgEq_pt0 ~ 0,
+      TRUE ~ 1
+      ),
+  )
+#----
+  
+  # Name of the year-specific subset for saving in global environment  
+  name_df <- paste0("subset_nonanti_", tax_year_vector[i])
+  
+  # Saving the year-specific subset to the global env
+  assign(name_df, df_subset, envir=globalenv())
+}
 
-nonanti_frgnxpns2016_2 <- nonanti_frgnxpns %>%
-  filter(tax_year == 2016) %>%
-  slice(-seq(0.5 * n())) %>%
-  group_by(ein, tax_year) %>%
-  slice_min(rtrn_timestmp) %>%
-  mutate(
-    totalXpns_2013 = totalXpns/(232.957/240.007),
-    frgnXpns_2013 = frgnXpns/(232.957/240.007))
-#--------
-#--------  
-# 2017
-#--------
-nonanti_frgnxpns2017_1 <- nonanti_frgnxpns %>%
-  filter(tax_year == 2017) %>%
-  slice(seq(0.5 * n())) %>%
-  group_by(ein, tax_year) %>%
-  slice_min(rtrn_timestmp) %>%
-  mutate(
-    totalXpns_2013 = totalXpns/(232.957/245.120),
-    frgnXpns_2013 = frgnXpns/(232.957/245.120))
+# Binding all the non-anti data frames together
+nonanti_frgnxpns_clean <- mget(ls(pattern="^subset_nonanti_")) %>%
+  bind_rows()
 
-nonanti_frgnxpns2017_2 <- nonanti_frgnxpns %>%
-  filter(tax_year == 2017) %>%
-  slice(-seq(0.5 * n())) %>%
-  group_by(ein, tax_year) %>%
-  slice_min(rtrn_timestmp) %>%
-  mutate(
-    totalXpns_2013 = totalXpns/(232.957/245.120),
-    frgnXpns_2013 = frgnXpns/(232.957/245.120))
-#--------
-#--------
-# 2018
-#--------
-nonanti_frgnxpns2018_1 <- nonanti_frgnxpns %>%
-  filter(tax_year == 2018) %>%
-  slice(seq(0.5 * n())) %>%
-  group_by(ein, tax_year) %>%
-  slice_min(rtrn_timestmp) %>%
-  mutate(
-    totalXpns_2013 = totalXpns/(232.957/251.107),
-    frgnXpns_2013 = frgnXpns/(232.957/251.107))
+summary(nonanti_frgnxpns_clean)
 
-nonanti_frgnxpns2018_2 <- nonanti_frgnxpns %>%
-  filter(tax_year == 2018) %>%
-  slice(-seq(0.5 * n())) %>%
-  group_by(ein, tax_year) %>%
-  slice_min(rtrn_timestmp) %>%
-  mutate(
-    totalXpns_2013 = totalXpns/(232.957/251.107),
-    frgnXpns_2013 = frgnXpns/(232.957/251.107))
-#--------
-#--------
-# 2019
-#--------
-nonanti_frgnxpns2019_1 <- nonanti_frgnxpns %>%
-  filter(tax_year == 2019) %>%
-  slice(seq(0.5 * n())) %>%
-  group_by(ein, tax_year) %>%
-  slice_min(rtrn_timestmp) %>%
-  mutate(
-    totalXpns_2013 = totalXpns/(232.957/255.657),
-    frgnXpns_2013 = frgnXpns/(232.957/255.657))
+#--------------------------
+# Exporting the data
+#-------------------------- 
+write_csv(anti_frgnxpns_clean, "/Users/srojascabal/Desktop/000_f990_data/anti_sample_dirty_220630.csv")
+write_csv(nonanti_frgnxpns_clean, "/Users/srojascabal/Desktop/000_f990_data/nonanti_sample_dirty_220630.csv")
+#--------------------------
 
-nonanti_frgnxpns2019_2 <- nonanti_frgnxpns %>%
-  filter(tax_year == 2019) %>%
-  slice(-seq(0.5 * n())) %>%
-  group_by(ein, tax_year) %>%
-  slice_min(rtrn_timestmp) %>%
+# Visualizations to see what else is wrong
+test <- nonanti_frgnxpns_clean %>%
   mutate(
-    totalXpns_2013 = totalXpns/(232.957/255.657),
-    frgnXpns_2013 = frgnXpns/(232.957/255.657))
-#--------
-#--------
-# 2020
-#--------
-nonanti_frgnxpns2020_1 <- nonanti_frgnxpns %>%
-  filter(tax_year == 2020) %>%
-  slice(seq(0.5 * n())) %>%
-  group_by(ein, tax_year) %>%
-  slice_min(rtrn_timestmp) %>%
-  mutate(
-    totalXpns_2013 = totalXpns/(232.957/258.811),
-    frgnXpns_2013 = frgnXpns/(232.957/258.811))
+    mm_totalXpns2013 = totalXpns_2013/1000000,
+    mm_frgnXpns2013 = frgnXpns_2013/1000000,
+    mm_pctg_FrngXpns2013 = (mm_frgnXpns2013/mm_totalXpns2013)*100
+  )
 
-nonanti_frgnxpns2020_2 <- nonanti_frgnxpns %>%
-  filter(tax_year == 2020) %>%
-  slice(-seq(0.5 * n())) %>%
-  group_by(ein, tax_year) %>%
-  slice_min(rtrn_timestmp) %>%
+test2 <- anti_frgnxpns_clean %>%
   mutate(
-    totalXpns_2013 = totalXpns/(232.957/258.811),
-    frgnXpns_2013 = frgnXpns/(232.957/258.811))
-#--------
+    mm_totalXpns2013 = totalXpns_2013/1000000,
+    mm_frgnXpns2013 = frgnXpns_2013/1000000,
+    mm_pctg_FrngXpns2013 = (frgnXpns_2013/totalXpns_2013)*100)
+  )
 
-nonanti_frgnxpns2 <- bind_rows(
-  nonanti_frgnxpns2013_1,
-  nonanti_frgnxpns2013_2,
-  nonanti_frgnxpns2014_1,
-  nonanti_frgnxpns2014_2,
-  nonanti_frgnxpns2015_1,
-  nonanti_frgnxpns2015_2,
-  nonanti_frgnxpns2016_1,
-  nonanti_frgnxpns2016_2,
-  nonanti_frgnxpns2017_1,
-  nonanti_frgnxpns2017_2,
-  nonanti_frgnxpns2018_1,
-  nonanti_frgnxpns2018_2,
-  nonanti_frgnxpns2019_1,
-  nonanti_frgnxpns2019_2,
-  nonanti_frgnxpns2020_1,
-  nonanti_frgnxpns2020_2
-)
+summary(test$mm_pctg_FrngXpns2013)
+
+#%>%
+ggplot(aes(x=mm_totalXpns2013)) + 
+  geom_density()
+
 
 #   how to calculate real 2013 dollars:
 #   (money in year X)/((CPI in 2013)/(CPI in that year))

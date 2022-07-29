33b4d8e35fa28534c21a567f3d528becf9db6ed3
Sebastian Rojas Cabal <srojascabal@gmail.com>
2022-07-29 12:30:16 -0400

    Models for ASA paper and presentation.

--- Code Files/3. Models_ASA Presentation.R
+++ Code Files/3. Models_ASA Presentation.R
@@ -1,27 +1,25 @@
 ## Project: Nonprofit Foreign Expenditures
 
 ## Overview: 
-#   This file produces diagnostics and other visualizations
-#   for the analysis presented in the ASA Annual Meeting.
+#   This file runs the models to be presented as results
+#   in ASA's annual meeting.
 
-## Last updated: July 27th by Sebastian Rojas Cabal
+## Last updated: July 29th by Sebastian Rojas Cabal
 #--------------------------------------------------------
 #--------------------------------------------------------
 # Loading packages
 #--------------------------------------------------------
 library(tidyverse)
-library(DescTools)
 library(lmtest)
 library(sandwich)
 library(stargazer)
-library(ggeffects)
 library(estimatr)
 library(aod)
 #--------------------------------------------------------
 #--------------------------------------------------------
 # Data Import
 #--------------------------------------------------------
-nonprofits_analysis <- read_csv("/Users/srojascabal/Desktop/000_f990_data/analytical_sample_220727.csv",
+nonprofits_analysis <- read_csv("/Users/srojascabal/Desktop/000_f990_data/analytical_sample_220729.csv",
                                 col_types = cols(
                                   ein = col_character(),
                                   tax_year = col_character(),
@@ -44,55 +42,42 @@ nonprofits_analysis <- read_csv("/Users/srojascabal/Desktop/000_f990_data/analyt
                                   gdp_state_2012 = col_double(),
                                   gdp_state_2012_100k = col_double(),
                                   gov_party = col_character(),
-                                  gov_republican = col_double()
+                                  gov_republican = col_double(),
+                                  gdp_state_2012 = col_double(),
+                                  gdp_state_2012_100k = col_double(),
+                                  gov_party = col_character(),
+                                  gov_republican = col_double(),
+                                  spends_abroad = col_double(),
+                                  winsor_totalXpns_2012_99 = col_double(),
+                                  winsor_totalXpns_2012_95 = col_double(),
+                                  winsor_frgnXpns_2012_99 = col_double(),
+                                  winsor_frgnXpns_2012_95 = col_double(),
+                                  log_gdp_2012 = col_double(),
+                                  log_gdp_2012_100k = col_double(),
+                                  log_frgnXpns_2012_100k_95 = col_double(),
+                                  log_frgnXpns_2012_100k_99 = col_double(),
+                                  log_totalXpns_2012_100k_95 = col_double(),
+                                  log_totalXpns_2012_100k_99 = col_double(),
+                                  log_rel_orgs = col_double(),
+                                  log_exempt_orgs = col_double()
                                 ))
-
 #--------------------------------------------------------
-# New Variables
-#   Indicator of whether or not the org spent money abroad that year
-#   Winsorized Total and Foreign Expenses (to the 95th and 99th percentile)
-#   Logged State GDP
-#   Logged Foreign Expenses
-#   Logged Total Expenses
-#   Logged # of Religious Organizations by State
-#   Logged # of Tax Exempt Organizations by State
-#--------------------------------------------------------
-nonprofits_analysis <- nonprofits_analysis %>%
-  mutate(
-    spends_abroad = case_when(
-      frgnXpns_2012_100k > 0 ~ 1,
-      frgnXpns_2012_100k == 0 ~ 0
-    ),
-    winsor_totalXpns_2012_99 = Winsorize(totalXpns_2012_100k, probs=c(0.00, 0.99)),
-    winsor_totalXpns_2012_95 = Winsorize(totalXpns_2012_100k, probs=c(0.00, 0.95)),
-    winsor_frgnXpns_2012_99 = Winsorize(frgnXpns_2012_100k, probs=c(0.00, 0.99)),
-    winsor_frgnXpns_2012_95 = Winsorize(frgnXpns_2012_100k, probs=c(0.00, 0.95)),
-    log_gdp_2012 = log(gdp_state_2012),
-    log_gdp_2012_100k = log(gdp_state_2012_100k),
-    log_frgnXpns_2012_100k_95 = log(winsor_frgnXpns_2012_95+1),
-    log_frgnXpns_2012_100k_99 = log(winsor_frgnXpns_2012_99+1),
-    log_totalXpns_2012_100k_95 = log(winsor_totalXpns_2012_95+1),
-    log_totalXpns_2012_100k_99 = log(winsor_totalXpns_2012_99+1),
-    log_rel_orgs = log(rel_orgs +1),
-    log_exempt_orgs = log(exempt_orgs+1)
-  )
-
 #--------------------------------------------------------
 # Models: Logged Gross Foreign Expenses
 #--------------------------------------------------------
 # No controls
-model1 <- lm(log_frgnXpns_2012_100k_99 ~ anti_lgbtq,
+model1_frgnXpns <- lm(log_frgnXpns_2012_100k_99 ~ anti_lgbtq,
                     data = nonprofits_analysis)
 
   # Adjust standard errors
-  model1_cov1         <- vcovHC(model1, type = "HC3")
+  model1_cov1         <- vcovHC(model1_frgnXpns, type = "HC3")
   model1_rse    <- sqrt(diag(model1_cov1))
   
   # Adjust F statistic 
   # wald_model1 <- waldtest(model1, vcov = model1_cov1)
 
 # Controls + interaction
-model2 <- lm(log_frgnXpns_2012_100k_99 ~ anti_lgbtq + 
+model2_frgnXpns_complete <- lm(log_frgnXpns_2012_100k_99 ~ anti_lgbtq + 
                       anti_lgbtq*ind_yearMrgEq_rtrn +
                       gov_republican +
                       state_population +
@@ -106,36 +91,28 @@ model2 <- lm(log_frgnXpns_2012_100k_99 ~ anti_lgbtq +
                     data = nonprofits_analysis)
 
   # Adjust standard errors
-  model2_cov1         <- vcovHC(model2, type = "HC3")
+  model2_cov1         <- vcovHC(model2_frgnXpns_complete, type = "HC3")
   model2_rse    <- sqrt(diag(model2_cov1))
   
   # Adjust F statistic 
   # wald_model2 <- waldtest(model2, vcov = model2_cov1)
 
-stargazer(model1, model2, type = "html",
-          se        = list(model1_rse, model2_rse),
-          omit.stat = "f",
-          out = "/Users/srojascabal/Desktop/000_f990_data/Results/results_ASA_grossXpns.html")
-
-# Manually adding F-Statistic
-#          add.lines = list(c("F Statistic (df = 3; 360)", "12.879***", "7.73***")))
-
 #--------------------------------------------------------
 # Models: % of Total Expenditures
 #--------------------------------------------------------
 # No controls
-model3 <- lm(propFrgnXpns_2012_100k ~ anti_lgbtq,
+model3_propXpns <- lm(propFrgnXpns_2012_100k ~ anti_lgbtq,
              nonprofits_analysis)
 
   # Adjust standard errors
-  model3_cov1         <- vcovHC(model3, type = "HC3")
+  model3_cov1         <- vcovHC(model3_propXpns, type = "HC3")
   model3_rse    <- sqrt(diag(model3_cov1))
   
   # Adjust F statistic 
   # wald_model3 <- waldtest(model3, vcov = model3_cov1)
 
 # % of Total Expenditures, controls + interaction
-model4 <- lm(propFrgnXpns_2012_100k ~ anti_lgbtq +
+model4_propXpns_complete <- lm(propFrgnXpns_2012_100k ~ anti_lgbtq +
                anti_lgbtq*ind_yearMrgEq_rtrn +
                gov_republican +
                state_population +
@@ -148,36 +125,31 @@ model4 <- lm(propFrgnXpns_2012_100k ~ anti_lgbtq +
              nonprofits_analysis)
 
   # Adjust standard errors
-  model4_cov1         <- vcovHC(model4, type = "HC3")
+  model4_cov1         <- vcovHC(model4_propXpns_complete, type = "HC3")
   model4_rse    <- sqrt(diag(model4_cov1))
   
   # Adjust F statistic 
   # wald_model4 <- waldtest(model4, vcov = model4_cov1)
 
-stargazer(model3, model4, type = "html",
-          se        = list(model3_rse, model4_rse),
-          omit.stat = "f",
-          out = "/Users/srojascabal/Desktop/000_f990_data/Results/results_ASA_percentXpns.html")
-
-# Manually adding F-Statistic
-#          add.lines = list(c("F Statistic (df = 3; 360)", "12.879***", "7.73***")))
+  # Manually adding F-Statistic
+  #          add.lines = list(c("F Statistic (df = 3; 360)", "12.879***", "7.73***")))
 
 #--------------------------------------------------------
 # Models: Likelihood of Start Spending Abroad (Logit)
 #--------------------------------------------------------
 # No controls
-model5 <- glm(spends_abroad ~ anti_lgbtq,
+model5_spendsAbrd <- glm(spends_abroad ~ anti_lgbtq,
               data = nonprofits_analysis, family = "binomial")
 
   # Adjust standard errors
-  model5_cov1         <- vcovHC(model5, type = "HC3")
+  model5_cov1         <- vcovHC(model5_spendsAbrd, type = "HC3")
   model5_rse    <- sqrt(diag(model5_cov1))
   
   # Adjust F statistic 
   # wald_model5 <- waldtest(model5, vcov = model5_cov1)
 
 # Controls + interaction
-model6 <- glm(spends_abroad ~ anti_lgbtq +
+model6_spendsAbrd_complete <- glm(spends_abroad ~ anti_lgbtq +
                 anti_lgbtq*ind_yearMrgEq_rtrn +
                 gov_republican +
                 state_population +
@@ -192,16 +164,11 @@ model6 <- glm(spends_abroad ~ anti_lgbtq +
              family = "binomial")
 
   # Adjust standard errors
-  model6_cov1         <- vcovHC(model6, type = "HC3")
+  model6_cov1         <- vcovHC(model6_spendsAbrd_complete, type = "HC3")
   model6_rse    <- sqrt(diag(model6_cov1))
   
   # Adjust F statistic 
   # wald_model6 <- waldtest(model6, vcov = model6_cov1)
-
-stargazer(model5, model6, type = "html",
-          se        = list(model5_rse, model6_rse),
-          omit.stat = "f",
-          out = "/Users/srojascabal/Desktop/000_f990_data/Results/results_ASA_probFrgnXpns.html")
 
 # Manually adding F-Statistic
 #          add.lines = list(c("F Statistic (df = 3; 360)", "12.879***", "7.73***")))
@@ -209,19 +176,25 @@ stargazer(model5, model6, type = "html",
 #--------------------------------------------------------
 # Exporting all Models
 #--------------------------------------------------------
-stargazer(model1, model2, model3, model4, model5, model6, type = "html",
-          se        = list(model1_rse, model2_rse, model3_rse, model4_rse, model5_rse, model6_rse),
-          omit.stat = "f",
-          single.row = TRUE,
-          out = "/Users/srojascabal/Desktop/000_f990_data/Results/results_ASA_allModels.html")
+# stargazer(model1_frgnXpns, model2_frgnXpns_complete,
+#           model3_propXpns, model4_propXpns_complete,
+#           model5_spendsAbrd, model6_spendsAbrd_complete,
+#           type = "html",
+#           se        = list(model1_rse, model2_rse, model3_rse, model4_rse, model5_rse, model6_rse),
+#           omit.stat = "f",
+#           single.row = TRUE,
+#           out = "/Volumes/GoogleDrive/My Drive/F990/foreign990/Results/results_ASA_allModels.html")
 #--------------------------------------------------------
 # Results for Paper
 #--------------------------------------------------------
-stargazer(model2, model4, model6, type = "html",
+stargazer(model2_frgnXpns_complete,
+          model4_propXpns_complete,
+          model6_spendsAbrd_complete,
+          type = "html",
           se        = list(model2_rse, model4_rse, model6_rse),
           omit.stat = c("f", "adj.rsq", "ser"),
           single.row = TRUE,
-          out = "/Users/srojascabal/Desktop/000_f990_data/Results/results_ASA_completeModels.html")
+          out = "/Volumes/GoogleDrive/My Drive/F990/foreign990/Results/results_ASA_completeModels.html")
 
 # Anti-LGBTQ
 exp(0.164)

2442b33a0de88cf6605ce7ab67c33763617828d1
Sebastian Rojas Cabal <srojascabal@gmail.com>
2022-07-29 12:30:39 -0400

    Figures for ASA paper and presentation.

--- Code Files/4. Figures_ASA Presentation.R
+++ Code Files/4. Figures_ASA Presentation.R
@@ -1,24 +1,24 @@
 ## Project: Nonprofit Foreign Expenditures
 
 ## Overview: 
-#   This file produces diagnostics and other visualizations
-#   for the analysis presented in the ASA Annual Meeting.
+#   This file produces the figures
+#   for the analyses presented in the ASA Annual Meeting.
 
-## Last updated: July 27th by Sebastian Rojas Cabal
+## Last updated: July 29th by Sebastian Rojas Cabal
 #--------------------------------------------------------
 #--------------------------------------------------------
 # Loading packages
 #--------------------------------------------------------
 library(tidyverse)
-library(DescTools)
 library(lmtest)
 library(sandwich)
 library(stargazer)
+library(estimatr)
 #--------------------------------------------------------
 #--------------------------------------------------------
 # Data Import
 #--------------------------------------------------------
-nonprofits_analysis <- read_csv("/Users/srojascabal/Desktop/000_f990_data/analytical_sample_220727.csv",
+nonprofits_analysis <- read_csv("/Users/srojascabal/Desktop/000_f990_data/analytical_sample_220729.csv",
                                 col_types = cols(
                                   ein = col_character(),
                                   tax_year = col_character(),
@@ -41,93 +41,171 @@ nonprofits_analysis <- read_csv("/Users/srojascabal/Desktop/000_f990_data/analyt
                                   gdp_state_2012 = col_double(),
                                   gdp_state_2012_100k = col_double(),
                                   gov_party = col_character(),
-                                  gov_republican = col_double()
+                                  gov_republican = col_double(),
+                                  gdp_state_2012 = col_double(),
+                                  gdp_state_2012_100k = col_double(),
+                                  gov_party = col_character(),
+                                  gov_republican = col_double(),
+                                  spends_abroad = col_double(),
+                                  winsor_totalXpns_2012_99 = col_double(),
+                                  winsor_totalXpns_2012_95 = col_double(),
+                                  winsor_frgnXpns_2012_99 = col_double(),
+                                  winsor_frgnXpns_2012_95 = col_double(),
+                                  log_gdp_2012 = col_double(),
+                                  log_gdp_2012_100k = col_double(),
+                                  log_frgnXpns_2012_100k_95 = col_double(),
+                                  log_frgnXpns_2012_100k_99 = col_double(),
+                                  log_totalXpns_2012_100k_95 = col_double(),
+                                  log_totalXpns_2012_100k_99 = col_double(),
+                                  log_rel_orgs = col_double(),
+                                  log_exempt_orgs = col_double()
                                 ))
-#--------------------------------------------------------
-# New Variables
-#   Indicator of whether or not the org spent money abroad that year
-#   Winsorized Total and Foreign Expenses (to the 95th and 99th percentile)
-#   Logged State GDP
-#   Logged Foreign Expenses
-#   Logged Total Expenses
-#   Logged # of Religious Organizations by State
-#   Logged # of Tax Exempt Organizations by State
-#--------------------------------------------------------
-nonprofits_analysis <- nonprofits_analysis %>%
-  mutate(
-    spends_abroad = case_when(
-      frgnXpns_2012_100k > 0 ~ 1,
-      frgnXpns_2012_100k == 0 ~ 0
-    ),
-    winsor_totalXpns_2012_99 = Winsorize(totalXpns_2012_100k, probs=c(0.00, 0.99)),
-    winsor_totalXpns_2012_95 = Winsorize(totalXpns_2012_100k, probs=c(0.00, 0.95)),
-    winsor_frgnXpns_2012_99 = Winsorize(frgnXpns_2012_100k, probs=c(0.00, 0.99)),
-    winsor_frgnXpns_2012_95 = Winsorize(frgnXpns_2012_100k, probs=c(0.00, 0.95)),
-    log_gdp_2012 = log(gdp_state_2012),
-    log_gdp_2012_100k = log(gdp_state_2012_100k),
-    log_frgnXpns_2012_100k_95 = log(frgnXpns_2013_100k+1),
-    log_frgnXpns_2012_100k_99 = log(winsor_frgnXpns_2012_95+1),
-    log_totalXpns_2012_100k_95 = log(winsor_totalXpns_2012_99+1),
-    log_totalXpns_2012_100k_99 = log(winsor_totalXpns_2012_95+1),
-    log_rel_orgs = log(rel_orgs +1),
-    log_exempt_orgs = log(exempt_orgs+1)
-  )
 #--------------------------------------------------------
 # Models
 #--------------------------------------------------------
-# Models without Fixed Effects Robust Standard Errors
-# Logged Foreign Expenses, no controls
-model1 <- lm(log_frgnXpns_2012_100k_95 ~ anti_lgbtq,
-             nonprofits_analysis)
+#--------------------------------------------------------
+# Models: Logged Gross Foreign Expenses
+#--------------------------------------------------------
+model_frgnXpns <- lm_robust(log_frgnXpns_2012_100k_99 ~ anti_lgbtq + 
+                                 anti_lgbtq*ind_yearMrgEq_rtrn +
+                                 gov_republican +
+                                 state_population +
+                                 frgn_born +
+                                 college_educ_over_25 +
+                                 log_gdp_2012 + 
+                                 ind_yearMrgEq_rtrn + 
+                                 log_exempt_orgs + 
+                                 log_rel_orgs + 
+                                 log_totalXpns_2012_100k_99,
+                               data = nonprofits_analysis)
 
-# Foreign Expenses, controls + interaction
-model2 <- lm(log_frgnXpns_2012_100k_95 ~ anti_lgbtq + 
-               anti_lgbtq*ind_yearMrgEq_rtrn +
-               gov_republican +
-               state_population +
-               frgn_born +
-               college_educ_over_25 +
-               log_gdp_2012 + 
-               ind_yearMrgEq_rtrn + 
-               log_exempt_orgs + 
-               log_rel_orgs + 
-               log_totalXpns_2012_100k_95,
-             nonprofits_analysis)
+tidy(model_frgnXpns)
 
-
-a <- coeftest(model2, vcovHC(model2, type = "HC3"))
-
-model2$fitted.values
-
-nonprofits_analysis <- nonprofits_analysis %>%
+fitted_frgnXnps <- nonprofits_analysis %>%
   mutate(
-    model2_fitted = model2$fitted.values
+    fit_frgnXnps = model_frgnXpns$fitted.values
+  ) %>%
+  select(
+    rtrn_state,
+    tax_year,
+    ind_yearMrgEq_rtrn,
+    anti_factor,
+    fit_frgnXnps
   )
 
-nonprofits_analysis %>%
+# 2013
+fitted_frgnXnps %>%
+  filter(
+    ind_yearMrgEq_rtrn
+  )
+  mutate(
+    factor_txyr = as.factor(tax_year)
+  ) %>%
+  group_by(factor_txyr, anti_factor) %>%
+  summarise(
+    expenses = mean(fit_frgnXnps)
+  ) %>%
   ggplot() +
-  geom_line(aes(x = tax_year, y = log_frgnXpns_2012_100k_95))  +
-  geom_line(aes(y = model2_fitted), size = 1)
+  geom_line(aes(x = factor_txyr, y = expenses, group = anti_factor, color = anti_factor)) +
+  geom_point(aes(x = factor_txyr, y = expenses, color = anti_factor)) +
+  labs(y = "Average Foreign Expenses \n(Hundreds of Thousands of USD)",
+       caption = "N (Non Anti-LGBTQ+): 1,636,682\nN (Anti-LGBTQ+): 5,608\nAmounts in real 2013 USD\nDashed line indicates the year same-sex marriage was legalized at the federal level") +
+  theme_bw() +
+  theme(legend.position = "bottom",
+        axis.title.x=element_blank(),
+        legend.title = element_blank()) +
+  geom_vline(xintercept = "2015", linetype = "dashed", color = "gray29")
 
-nonprofits_analysis %>%
-  ggplot(aes(x = tax_year, y = log_frgnXpns_2012_100k_95, color = anti_factor) ) +
-  geom_point() +
-  geom_line(aes(y = model2_fitted), size = 1)
+ggsave("/Users/srojascabal/Desktop/000_f990_data/Plots/gross_national.png",
+       width = 210,
+       height = 145, #A5 (half of A4)
+       units = "mm",
+       dpi = "print"
+)
 
+# 2014
 
-# % of Total Expenditures, no controls
-model3 <- lm(propFrgnXpns_2012_100k ~ anti_lgbtq,
-             nonprofits_analysis)
+# 2015
+
+# Adjust standard errors
+model2_cov1         <- vcovHC(model2_frgnXpns_complete, type = "HC3")
+model2_rse    <- sqrt(diag(model2_cov1))
+
+# Adjust F statistic 
+# wald_model2 <- waldtest(model2, vcov = model2_cov1)
+
+#--------------------------------------------------------
+# Models: % of Total Expenditures
+#--------------------------------------------------------
+# No controls
+model3_propXpns <- lm(propFrgnXpns_2012_100k ~ anti_lgbtq,
+                      nonprofits_analysis)
+
+# Adjust standard errors
+model3_cov1         <- vcovHC(model3_propXpns, type = "HC3")
+model3_rse    <- sqrt(diag(model3_cov1))
+
+# Adjust F statistic 
+# wald_model3 <- waldtest(model3, vcov = model3_cov1)
 
 # % of Total Expenditures, controls + interaction
-model4 <- lm(propFrgnXpns_2012_100k ~ anti_lgbtq +
-               anti_lgbtq*ind_yearMrgEq_rtrn +
-               gov_republican +
-               state_population +
-               frgn_born +
-               college_educ_over_25 +
-               log_gdp_2012 +
-               ind_yearMrgEq_rtrn +
-               log_exempt_orgs +
-               log_rel_orgs,
-             nonprofits_analysis)
+model4_propXpns_complete <- lm(propFrgnXpns_2012_100k ~ anti_lgbtq +
+                                 anti_lgbtq*ind_yearMrgEq_rtrn +
+                                 gov_republican +
+                                 state_population +
+                                 frgn_born +
+                                 college_educ_over_25 +
+                                 log_gdp_2012 +
+                                 ind_yearMrgEq_rtrn +
+                                 log_exempt_orgs +
+                                 log_rel_orgs,
+                               nonprofits_analysis)
+
+# Adjust standard errors
+model4_cov1         <- vcovHC(model4_propXpns_complete, type = "HC3")
+model4_rse    <- sqrt(diag(model4_cov1))
+
+# Adjust F statistic 
+# wald_model4 <- waldtest(model4, vcov = model4_cov1)
+
+# Manually adding F-Statistic
+#          add.lines = list(c("F Statistic (df = 3; 360)", "12.879***", "7.73***")))
+
+#--------------------------------------------------------
+# Models: Likelihood of Start Spending Abroad (Logit)
+#--------------------------------------------------------
+# No controls
+model5_spendsAbrd <- glm(spends_abroad ~ anti_lgbtq,
+                         data = nonprofits_analysis, family = "binomial")
+
+# Adjust standard errors
+model5_cov1         <- vcovHC(model5_spendsAbrd, type = "HC3")
+model5_rse    <- sqrt(diag(model5_cov1))
+
+# Adjust F statistic 
+# wald_model5 <- waldtest(model5, vcov = model5_cov1)
+
+# Controls + interaction
+model6_spendsAbrd_complete <- glm(spends_abroad ~ anti_lgbtq +
+                                    anti_lgbtq*ind_yearMrgEq_rtrn +
+                                    gov_republican +
+                                    state_population +
+                                    frgn_born +
+                                    college_educ_over_25 +
+                                    log_gdp_2012 + 
+                                    ind_yearMrgEq_rtrn + 
+                                    log_exempt_orgs + 
+                                    log_rel_orgs + 
+                                    log_totalXpns_2012_100k_99,
+                                  data = nonprofits_analysis,
+                                  family = "binomial")
+
+# Adjust standard errors
+model6_cov1         <- vcovHC(model6_spendsAbrd_complete, type = "HC3")
+model6_rse    <- sqrt(diag(model6_cov1))
+
+# Adjust F statistic 
+# wald_model6 <- waldtest(model6, vcov = model6_cov1)
+
+# Manually adding F-Statistic
+#          add.lines = list(c("F Statistic (df = 3; 360)", "12.879***", "7.73***")))

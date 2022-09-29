2f0fcfbe1d63bf39cd7ae0129834ad4aaccf1928
Sebastian Rojas Cabal <srojascabal@gmail.com>
2022-09-29 12:02:10 -0500

    Raw location data from Schedule F.

--- Code Files/000. Recipient Country Info.R
+++ Code Files/000. Recipient Country Info.R
@@ -4,6 +4,7 @@
 #   This file uses Schedule F to find the location (country, region) of recipients of foreign grants by U.S.-based nonprofits.
 
 ## Output:
+#   locations_raw.csv
 
 ## Last updated: Sept. 29th by Sebastian Rojas Cabal
 #--------------------------------------------------------
@@ -39,4 +40,31 @@ dirty_f_individualGrants <- read_csv("/Users/srojascabal/Desktop/000_f990_data/s
 # Supplement
 # NO INFO
 dirty_f_supplement <- read_csv("/Users/srojascabal/Desktop/000_f990_data/sched_f_supplement.csv")
+
+#--------------------------
+# Appending region info from activities and individual grants data
+#--------------------------
+activities <- dirty_f_activities %>%
+  select(RgnTxt) %>%
+  distinct()
+
+grants <- dirty_f_individualGrants %>%
+  select(RgnTxt) %>%
+  distinct()
+
+regions_raw <- bind_rows(activities, grants) %>%
+  mutate(
+    RgnTxt = str_to_title(RgnTxt, locale = "en"),
+    ) %>%
+  rename(f_location = RgnTxt) %>%
+  distinct() %>%
+  mutate(
+    length = str_length(f_location)
+  ) %>%
+  arrange(desc(length))
+
+#--------------------------
+# Exporting locations_raw.csv
+#--------------------------
+write_csv(regions_raw, "/Users/srojascabal/Desktop/000_f990_data/locations_raw.csv")
 
